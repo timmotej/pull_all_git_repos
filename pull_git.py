ee63c6b3efa4c23679e8ef945e6e6bd67ea20629
timo <timmotej@gmail.com>
2019-01-18 15:47:53 +0100

    pull_git.py plan for new script for pulling requests for git

---
+++ pull_git.py
@@ -0,0 +1,21 @@
+#!/bin/env python3
+
+import sys
+import os
+
+def spider_dir_tree():
+	#go to git dir
+	#list dir
+	#go from list to dir
+
+def git_pull_request():
+	if # there is a dir with name .git, try pull request
+
+def main():
+	spider_dir_tree(git_pull_request())
+
+if __name__ == "__main__":
+	main()
+	git_pull_request()
+	spider_dir_tree()
+	
