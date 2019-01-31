810581076a0463195f849d896cad4b00c54fc626
Tomas Stribula <stribula@ics.muni.cz>
2019-01-31 15:55:45 +0100

    pull_git.py started with functions ls and pwd

--- pull_git.py
+++ pull_git.py
@@ -2,21 +2,27 @@
 
 import sys
 import os
+import git
 
 # add root file for git repos
 def spider_dir_tree():
+	return os.getcwd().split("/")[1:]
 	#go to git dir
 	#list dir
 	#go from list to dir
 
 def git_pull_request():
-	if # there is a dir with name .git, try pull request
+	print(os.listdir())
+	g = git.cmd.Git(os.getcwd())
+	g.pull()
+	# there is a dir with name .git, try pull request
 
 def main():
-	spider_dir_tree(git_pull_request())
+	pass
+	#spider_dir_tree(git_pull_request())
 
 if __name__ == "__main__":
 	main()
 	git_pull_request()
-	spider_dir_tree()
+	print(spider_dir_tree())
 	
