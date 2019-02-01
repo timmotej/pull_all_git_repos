ccfde0b33ac1a655cd5f9870da9544d5914c58e0
timo <timmotej@gmail.com>
2019-02-01 07:14:35 +0100

    pull_git.py

--- pull_git.py
+++ pull_git.py
@@ -11,6 +11,21 @@ def spider_dir_tree():
 	#list dir
 	#go from list to dir
 
+a = spider_dir_tree()
+
+def gen_list(list_to_cut, to_find):
+	for i in range(len(list_to_cut)):
+		yield list_to_cut[i]
+		i+=1
+		if list_to_cut[i] == to_find:
+			break
+
+def get_path_to_dir(to_dir):
+	return [next(genlist(spider_dir_tree(),to_dir))]
+
+print(a)
+print(get_path_to_dir("git"))
+
 def git_pull_request():
 	print(os.listdir())
 	g = git.cmd.Git(os.getcwd())
